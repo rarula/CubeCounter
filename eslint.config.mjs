4c8effa9dc7d979b8712d07407ee061ca7ecbf56
rarula <74240663+rarula@users.noreply.github.com>
2025-01-17 19:36:03 +0900

    format eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,13 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tsEslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tsEslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-    {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-    {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-    {languageOptions: { globals: globals.browser }},
+    { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { languageOptions: { globals: globals.browser } },
     pluginJs.configs.recommended,
     ...tsEslint.configs.recommended,
     pluginReact.configs.flat.recommended,
