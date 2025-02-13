46b01f349935c897b639c38d642dc465776b9665
rarula <74240663+rarula@users.noreply.github.com>
2025-02-13 20:59:59 +0900

    add how to play

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,26 +2,29 @@ import { useState } from 'react';
 
 import CalendarModal from './CalendarModal';
 import Fade from './Fade';
-import { CalendarSvg } from './Svg';
+import { CalendarSvg, CrossSvg } from './Svg';
 import { useGameState } from '../contexts/GameState';
 import { toDateString } from '../core/utils';
 
 const Menu = (): JSX.Element => {
     const state = useGameState();
-    const [isOpened, setOpened] = useState(false);
+    const [isCalendarOpened, setCalendarOpened] = useState(false);
+    const [isHowToPlayOpened, setHowToPlayOpened] = useState(false);
 
     const dateStr = toDateString(state.date);
 
     const openCalendar = (): void => {
-        setOpened(true);
+        setCalendarOpened(true);
     };
-
     const closeCalendar = (): void => {
-        setOpened(false);
+        setCalendarOpened(false);
     };
 
-    const handleClick = (): void => {
-        state.setPlaying(true);
+    const openHowToPlay = (): void => {
+        setHowToPlayOpened(true);
+    };
+    const closeHowToPlay = (): void => {
+        setHowToPlayOpened(false);
     };
 
     return (
@@ -34,15 +37,40 @@ const Menu = (): JSX.Element => {
                         <div className='icon-button calendar-button' onClick={openCalendar}>
                             <CalendarSvg />
                         </div>
-                        <div className='button' onClick={handleClick}>
+                        <div className='button' onClick={() => state.setPlaying(true)}>
                             PLAY
                         </div>
                     </div>
-                    <div className='button'>HOW TO PLAY</div>
+                    <div className='button' onClick={openHowToPlay}>
+                        HOW TO PLAY
+                    </div>
                 </div>
             </div>
-            <Fade duration='0.2s' in={isOpened}>
-                <CalendarModal closeCalendar={closeCalendar} isOpened={isOpened} />
+            <Fade duration='0.2s' in={isCalendarOpened}>
+                <CalendarModal closeCalendar={closeCalendar} isOpened={isCalendarOpened} />
+            </Fade>
+            <Fade duration='0.2s' in={isHowToPlayOpened}>
+                <div className='modal-background' onClick={closeHowToPlay} />
+                <div className='modal how-to-play'>
+                    <div className='htp-close' onClick={closeHowToPlay}>
+                        <CrossSvg />
+                    </div>
+                    <h2 className='text-center color-gray'>How To Play</h2>
+                    <div className='htp'>
+                        <img className='htp-img' src='/how-to-play.png' />
+                        <ol className='htp-letter-list'>
+                            <li className='htp-letter'>
+                                <span className='color-gray'>画面上に表示される立方体の総数を数えましょう。隠れていて見えない位置の立方体も考慮することに注意してください。</span>
+                            </li>
+                            <li className='htp-letter'>
+                                <span className='color-gray'>正しい総数を示しているボタンを押して解答します。正解すると次の問題に移ります。</span>
+                            </li>
+                            <li className='htp-letter'>
+                                <span className='color-gray'>問題は全部で5つです。すべての問題に正解すると終了します。</span>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
             </Fade>
         </>
     );
--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -34,6 +34,17 @@ export const RightArrowSvg = (): JSX.Element => {
     );
 };
 
+export const CrossSvg = (): JSX.Element => {
+    return (
+        <svg viewBox='0 0 15 15' xmlns='http://www.w3.org/2000/svg'>
+            <path
+                d='M6.7929 7.49998L1.14645 1.85353L1.85356 1.14642L7.50001 6.79287L13.1465 1.14642L13.8536 1.85353L8.20711 7.49998L13.8536 13.1464L13.1465 13.8535L7.50001 8.20708L1.85356 13.8535L1.14645 13.1464L6.7929 7.49998Z'
+                fill='currentColor'
+            ></path>
+        </svg>
+    );
+};
+
 export const TwitterSvg = (): JSX.Element => {
     return (
         <svg xmlns='http://www.w3.org/2000/svg' width='100%' viewBox='0 0 64 64'>
